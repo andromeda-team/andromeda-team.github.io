c7d523dd4048e92ea8d2c301ee47655c96e9c7cc
bubblefish-dev <daforsastudia@gmail.com>
2021-07-07 22:12:40 +0300

    Changed JSON Gradients + enhanced on phones

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ let navigation = document.getElementById('_navigation');
 let mobileButton = document.getElementById('_mobile-button');
 let isMobileButtonActive = false;
 
-mobileButton.onclick = function () {
+mobileButton.onclick = function() {
     isMobileButtonActive = !isMobileButtonActive;
-    if (isMobileButtonActive) {
+    updateNavbar(isMobileButtonActive);
+};
+
+window.onresize = function() {
+    if (window.innerWidth > 900) {
+        updateNavbar(true)
+    } else {
+        updateNavbar(false)
+    }
+};
+
+function updateNavbar(boolean) {
+    if (boolean) {
         navigation.style.setProperty('left', '0');
         mobileButton.style.setProperty('position', 'absolute');
         mobileButton.style.setProperty('left', 'calc(100% - 5em)');
         mobileButton.children[0].style.setProperty('transform', 'translateX(-96px)');
         mobileButton.children[1].style.setProperty('left', '4px');
+        navigation.style.setProperty('display', 'flex');
     } else {
         mobileButton.style.setProperty('position', 'relative');
         mobileButton.style.setProperty('left', '0');
@@ -17,5 +30,8 @@ mobileButton.onclick = function () {
         mobileButton.style.setProperty('transform', 'translateX(0)');
         mobileButton.children[0].style.setProperty('transform', 'translateX(0)');
         mobileButton.children[1].style.setProperty('left', '96px');
+        setTimeout(function() {
+            navigation.style.setProperty('display', 'none');
+        }, 200);
     }
 };
