6d9b2f6b99d518c76831c4cfc7de5533aa918add
IAmRuMaks <rumaks.mail@protonmail.com>
2022-03-18 19:51:33 +0300

    Remake deploy.py

--- deploy.py
+++ deploy.py
@@ -1,42 +1,37 @@
-#! Run this script using Python 3.x before merging a request/pushing if a navbar change was made or a new page created.
+# ONLY WORKS IN PYTHON 3.9 AND NEWER
 
-from time import sleep
-import os
+from pathlib import Path
 
-def deploy(include_mode: True):
-    # Include the navbar into other HTML files:
-    html_files = [os.path.join(r, fn)
-        for r, _, fs in os.walk(os.getcwd()) 
-        for fn in fs if fn.endswith('.html')]
-
-    for html_file in html_files:
-        if not html_file.endswith(('navbar.html', 'tabs.html')):
-            include_html(html_file, include_mode, 'navbar')
-            include_html(html_file, include_mode, 'tabs')
+HTML_PARSE_BLACKLIST = ("assets",)
 
 
-def include_html(html_file, include_mode, string):
-    capitalized = string.upper()
-    with open(html_file, 'r') as file:
-            lines = file.readlines()
-            bounds = [-1] * 2
-            for index, line in enumerate(lines):
-                if f'<!-- INCLUDE {capitalized} -->' in line:
-                    bounds[0] = index
-                elif f'<!-- END INCLUDING {capitalized} -->' in line:
-                    bounds[1] = index
+def parse_html_files(): # Note: this function returns an iterator
+    return (str(path) for path in Path(".").rglob("*.html") if not str(path).startswith(HTML_PARSE_BLACKLIST))
 
-    with open(os.getcwd() + f'/assets/.{string}.html', 'r') as navbar:
-        navbar_lines = navbar.readlines()
 
-    with open(html_file, 'w') as file:
-        for index, line in enumerate(lines):
-            if index <= bounds[0] or index > bounds[1]:
-                file.write(line)
-                if include_mode and f'<!-- INCLUDE {capitalized} -->' in line:
-                    for navbar_line in navbar_lines:
-                        file.write(navbar_line)
+def include_html(path):
+    lines_old = open(path, "r").readlines()
+    lines_new = []
+    append_lines = True
+
+    for line in lines_old:
+
+        if append_lines:
+            if line.lstrip().startswith("<!-- INCLUDE"):
+                append_lines = False
+                include_name = line.strip().removeprefix("<!-- INCLUDE ").removesuffix(" -->").lower()
+            lines_new.append(line)
+
+        else:
+            if line.lstrip().startswith("<!-- END INCLUDING"):
+                lines_new.extend(open("assets/.{0}.html".format(include_name), "r").readlines())
+                append_lines = True
+                lines_new.append(line)
+
+    open(path, "w").writelines(lines_new)
 
 
 if __name__ == '__main__':
-    deploy(True if input('Would you like to run in INCLUDE mode? [Y/n] ').capitalize() == 'Y' else False)
+    if input('Would you like to run in INCLUDE mode? [Y/n] ').upper() == "Y":
+        for path in parse_html_files():
+            include_html(path)
