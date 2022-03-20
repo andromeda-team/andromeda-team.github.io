7d81d2282f243e02ebc92102a08c7871574c0763
IAmRuMaks <rumaks.mail@protonmail.com>
2022-03-20 14:36:56 +0300

    Update icons, fix viewboxing in svgs, improve the INCLUDE system

--- deploy.py
+++ deploy.py
@@ -1,15 +1,18 @@
 # ONLY WORKS IN PYTHON 3.9 AND NEWER
 
 from pathlib import Path
+import itertools
 
-HTML_PARSE_BLACKLIST = ("assets",)
-
+HTML_PARSE_BLACKLIST = ()
+html_files = []
 
 def parse_html_files(): # Note: this function returns an iterator
     return (str(path) for path in Path(".").rglob("*.html") if not str(path).startswith(HTML_PARSE_BLACKLIST))
 
 
 def include_html(path):
+    global html_files
+
     lines_old = open(path, "r").readlines()
     lines_new = []
     append_lines = True
@@ -19,12 +22,15 @@ def include_html(path):
         if append_lines:
             if line.lstrip().startswith("<!-- INCLUDE"):
                 append_lines = False
-                include_name = line.strip().removeprefix("<!-- INCLUDE ").removesuffix(" -->").lower()
+                include_path = line.strip().removeprefix("<!-- INCLUDE ").removesuffix(" -->").lower()
+                spaces_amount = len(tuple(itertools.takewhile(lambda x: x == " ", line)))
             lines_new.append(line)
 
         else:
-            if line.lstrip().startswith("<!-- END INCLUDING"):
-                lines_new.extend(open("assets/.{0}.html".format(include_name), "r").readlines())
+            if line.lstrip().startswith("<!-- END INCLUDING " + include_path):
+                if include_path in html_files:
+                    include_html(include_path)
+                lines_new.extend((" " * spaces_amount + i for i in open(include_path).readlines()))
                 append_lines = True
                 lines_new.append(line)
 
@@ -33,5 +39,6 @@ def include_html(path):
 
 if __name__ == '__main__':
     if input('Would you like to run in INCLUDE mode? [Y/n] ').upper() == "Y":
-        for path in parse_html_files():
-            include_html(path)
+        html_files = list(parse_html_files())
+        while len(html_files) > 0:
+            include_html(html_files.pop())
