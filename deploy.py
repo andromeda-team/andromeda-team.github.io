f821d317e90e3279393eb0740f935b7a93e18270
IAmRuMaks <rumaks.mail@protonmail.com>
2022-03-22 21:05:15 +0300

    Add "purge" mode for easier editing

--- deploy.py
+++ deploy.py
@@ -10,7 +10,7 @@ def parse_html_files(): # Note: this function returns an iterator
     return (str(path) for path in Path(".").rglob("*.html") if not str(path).startswith(HTML_PARSE_BLACKLIST))
 
 
-def include_html(path):
+def include_html(path, include_mode=True):
     global html_files
 
     lines_old = open(path, "r").readlines()
@@ -23,14 +23,16 @@ def include_html(path):
             if line.lstrip().startswith("<!-- INCLUDE"):
                 append_lines = False
                 include_path = line.strip().removeprefix("<!-- INCLUDE ").removesuffix(" -->").lower()
-                spaces_amount = len(tuple(itertools.takewhile(lambda x: x == " ", line)))
+                if include_mode:
+                    spaces_amount = len(tuple(itertools.takewhile(lambda x: x == " ", line)))
             lines_new.append(line)
 
         else:
             if line.lstrip().startswith("<!-- END INCLUDING " + include_path):
-                if include_path in html_files:
-                    include_html(include_path)
-                lines_new.extend((" " * spaces_amount + i for i in open(include_path).readlines()))
+                if include_mode:
+                    if include_path in html_files:
+                        include_html(include_path)
+                    lines_new.extend((" " * spaces_amount + i for i in open(include_path).readlines()))
                 append_lines = True
                 lines_new.append(line)
 
@@ -38,8 +40,9 @@ def include_html(path):
     html_files.remove(path)
 
 
-if __name__ == '__main__':
-    if input('Would you like to run in INCLUDE mode? [Y/n] ').upper() == "Y":
+if __name__ == "__main__":
+    option = input("[i] - INCLUDE mode\n[p] - PURGE mode\n[c] - CANCEL\nSelect mode: [i/p/c] ").lower()
+    if option == "i" or option == "p":
         html_files = list(parse_html_files())
         while len(html_files) > 0:
-            include_html(html_files[0])
+            include_html(html_files[0], option == "i")
